361443ccb53992b1e622ccd5f47efdc1550187ea
Navaseelan <navaseelan4u@gmail.com>
2017-09-14 11:22:59 +0530

    style changes

--- SSIT_SHR.Web/src/app/components/employee/employee-detail/employee-detail.component.ts
+++ SSIT_SHR.Web/src/app/components/employee/employee-detail/employee-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, Renderer2, Output } from '@angular/core';
+import {Component, ViewChild, ElementRef, Renderer2, Output, Injector} from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
@@ -13,13 +13,41 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./employee-detail.component.scss'],
     animations: [routerTransition()]
 })
+
 export class EmployeeDetail {
+    bloodType: Array<any>;
+    religion: Array<any>;
+    married: Array<any>;
+    country: Array<any>;
+
+    currentMenuTranslation: any;
+    router: Router;
     constructor(private employeeService: EmployeeService, private route: ActivatedRoute, private render2: Renderer2) {
 
     }
 
     ngOnInit() {
+
         let currentUser = localStorage.getItem('currentUser.UserID');
+        let Menutranslationfile = localStorage.getItem("currentUser.translationfile");
+        if (Menutranslationfile != null) {
+            this.currentMenuTranslation = JSON.parse(Menutranslationfile);
+            for(let i =0; i < this.currentMenuTranslation.result[0].rSreturnJSONFile.tc.length; i++ ){
+                if(this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].id == 13){
+                    this.bloodType = this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].values;
+                }else if(this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].id == 5){
+                    this.religion = this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].values;
+                }else if(this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].id == 11){
+                    this.married = this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].values;
+                }else if(this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].id == 12){
+                    this.country = this.currentMenuTranslation.result[0].rSreturnJSONFile.tc[i].values;
+                }
+            }
+            console.info(this.bloodType);
+
+        }
+
+
         if (this.route.snapshot.params['id']) {
             let id = this.route.snapshot.params['id'];
             let name = this.route.snapshot.params['name'];
@@ -33,9 +61,9 @@ export class EmployeeDetail {
     }
 
     ngAfterViewInit() {
-        console.log("tabbingData", this.tabbingData);
+        // console.log("tabbingData", this.tabbingData);
         this.render2.addClass(document.querySelector(".tab-content"), "tabing");
-        console.log('tab Data', document.querySelector(".tab-content"))
+        // console.log('tab Data', document.querySelector(".tab-content"))
     }
 
     @ViewChild('tabbingData') tabbingData: ElementRef;
@@ -113,13 +141,13 @@ export class EmployeeDetail {
     }
 
     profileEvent(event: any) {
-        console.log("Event", event.srcElement.files[0]);
+        // console.log("Event", event.srcElement.files[0]);
         let file = event.srcElement.files[0];
         let contenido;
         let fr = new FileReader();
         fr.onload = (e) => {
             this.employeeMaster.picture = fr.result.split(',')[1];
-            console.log(this.employeeMaster.picture);
+            // console.log(this.employeeMaster.picture);
         };
         fr.readAsDataURL(file);
     }
@@ -145,7 +173,7 @@ export class EmployeeDetail {
         this.employeeService.getEmployeeDetail(this.employeeDetail).map(res => res.json())
             .subscribe(res => {
                 this.finalData = res.result.employeeDetail.employee.forEach(element => {
-                    console.log("Element", element);
+                    // console.log("----Element---------", element);
                     this.employeeMaster.fullName = element.fullName;
                     this.employeeMaster.givenName = element.givenName;
                     this.employeeMaster.surname = element.surname;
@@ -165,7 +193,7 @@ export class EmployeeDetail {
                     this.dependent = element.dependent;
                     this.employment = element.employment;
                     this.payRollData = element.employment;
-                    console.log("this.payRollData",this.payRollData);
+                    // console.log("this.payRollData",this.payRollData);
                     this.insurance = element.insurance;
                     this.residentialDocument = element.residentialDocument;
                     this.employeeMaster.id = element.id;
